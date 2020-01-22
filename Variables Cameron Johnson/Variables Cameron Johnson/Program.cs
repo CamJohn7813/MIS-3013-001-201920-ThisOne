82e2d2b9e9e3597d45593c790ba9330400f94109
John7813 <John7813@PCB-LAB360-04>
2020-01-22 13:08:53 -0600

    changes

--- Variables Cameron Johnson/Variables Cameron Johnson/Program.cs
+++ Variables Cameron Johnson/Variables Cameron Johnson/Program.cs
@@ -10,6 +10,34 @@ namespace Variables_Cameron_Johnson
     {
         static void Main(string[] args)
         {
+            //create a string named firstName
+            string firstName;
+
+            //assign a value to firstName (use your name)
+            firstName = "Cameron";
+
+            //create an int named faavoriteNumber and assign your favorite number to it
+            int favoriteNumber = 27;
+
+            double crazyNumber = 8.3784 * favoriteNumber;
+
+            string lastName = "Johnson";
+            string fullName = firstName + lastName;
+
+            Console.WriteLine(fullName + "has the favorite number of " + favoriteNumber + " which equates to a crazy number of " + crazyNumber);
+
+            //MAKE IT DYNAMIC
+            Console.WriteLine("Please enter your first name: >>");
+
+            Console.WriteLine("Please enter your last name: >>");
+            Console.WriteLine("Please enter your favorite number: >>");
+            
+
+            Console.WriteLine(fullName + "has the favorite number of " + favoriteNumber + " which equates to a crazy number of " + crazyNumber);
+
+
+
+            Console.ReadKey();
         }
     }
 }
