8b516170175c2799403bfb7c7e287654da41e444
Cameron Johnson <camjohn@ou.edu>
2020-01-27 13:08:34 -0600

    class work

---
+++ InputFormattingConverting/InputFormattingConverting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputFormattingConverting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name >>");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your favorite number >>");
+            int favNumber = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
